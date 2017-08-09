d0228747ce22a0f76a0cbcdf3d89a1680092cca8
Elrey Belmonti and Kristen Kulha <elrey@>
2017-08-09 14:43:38 -0700

    Add color selector.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
     var dietaryInput = $("select#dietary").val();
     var drinkInput = $("input:radio[name=drink]:checked").val();
     var eventDateInput = $("#event-date").val();
+    var colorInput = $("#color").val();
     debugger;
     $(".name").text(nameInput);
     $(".dietary").text(dietaryInput);
     $(".drink").text(drinkInput);
     $(".event-date").text(eventDateInput);
+    $(".color").text(colorInput);
 
     $("#results").show();
 
