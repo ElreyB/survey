6dc91d6ae4c067a9f483c1fe6202078fbbd4ced4
Elrey Belmonti and Kristen Kulha <kristen@>
2017-08-09 14:37:22 -0700

    add form for user to select radio button and date.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   $("#survey-form").submit(function() {
     var nameInput = $("input#name").val();
     var dietaryInput = $("select#dietary").val();
+    var drinkInput = $("input:radio[name=drink]:checked").val();
+    var eventDateInput = $("#event-date").val();
     debugger;
     $(".name").text(nameInput);
     $(".dietary").text(dietaryInput);
+    $(".drink").text(drinkInput);
+    $(".event-date").text(eventDateInput);
 
     $("#results").show();
 
